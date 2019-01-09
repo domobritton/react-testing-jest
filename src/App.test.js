3277cf2a6362d045dabcc855a991b1a653c489ba
domobritton <domonicbritton@gmail.com>
2019-01-08 23:05:33 -0800

    First jest tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { total } from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+// mock function
+const add = jest.fn(() => 3);
+
+// unit test
+test('add', () => {
+    // const value = add(1, 2);
+    // expect(value).toBe(3);
+    expect(add(1, 2)).toBe(3);
+    expect(add).toHaveBeenCalledTimes(1);
+    // expect(add(5, 2)).toBe(7);
+});
+
+// integration test 
+test('total', () => {
+  expect(total(5, 20)).toBe('$25');
 });
